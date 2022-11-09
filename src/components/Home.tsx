54586540bd3c140bd7941dfa04c5842929c62bfe
mynkit <dcm5124289@gmail.com>
2022-11-09 10:19:15 +0900

    title追加

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Home = () => {
+  useEffect(() => {
+    document.title = '確率の検証アプリ';
+  }, [])
   return (
     <div>Home</div>
   )
