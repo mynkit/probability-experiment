120d26444a18407cdbb49b70cb46cadd619d37a0
mynkit <dcm5124289@gmail.com>
2022-12-03 21:04:41 +0900

    display kujibiki box

--- src/components/Lottery.tsx
+++ src/components/Lottery.tsx
@@ -1,8 +1,14 @@
 import React, { useState, useEffect } from 'react'
+import Grid from '@mui/material/Grid';
 
 const Lottery = () => {
   return (
-    <div>Lottery</div>
+    <div style={{padding: '10px'}}>
+      <h1>くじ引きの確率</h1>
+      <Grid container alignItems='center' justifyContent='center'>
+        <img src='/lotteries/kujibiki_box.png' width='50%'/>
+      </Grid>
+    </div>
   )
 }
 
