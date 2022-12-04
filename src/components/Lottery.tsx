6294afbcb8a6ccd2443d0cd4c22f73992a4378a2
mynkit <dcm5124289@gmail.com>
2022-12-04 17:49:48 +0900

    参加人数、くじ本数、当たり数を変更できるようにした

--- src/components/Lottery.tsx
+++ src/components/Lottery.tsx
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
 
 const Lottery = () => {
   const [personCount, setPersonCount] = useState(5);
@@ -12,10 +15,46 @@ const Lottery = () => {
   }
 
   return (
-    <div style={{padding: '10px'}}>
+    <div style={{padding: '10px', width: '700px', maxWidth: '100%'}}>
       <h1>くじ引きの確率</h1>
       <div>
-        <span>{`参加人数：${personCount}人`}</span>
+        <span style={{paddingRight: '10px'}}>{`参加人数：${personCount}人`}</span>
+        <Button color='inherit' variant="outlined" startIcon={<RemoveIcon />} onClick={()=>{
+          if (personCount>=3)setPersonCount(v=>v-1);
+        }}>
+          参加人数-1
+        </Button>
+        <Button color='inherit' variant="outlined" startIcon={<AddIcon />} onClick={()=>{
+          setPersonCount(v=>v+1);
+        }}>
+          参加人数+1
+        </Button>
+      </div>
+      <div>
+        <span style={{paddingRight: '10px'}}>{`くじ本数：${kujiCount}個`}</span>
+        <Button color='inherit' variant="outlined" startIcon={<RemoveIcon />} onClick={()=>{
+          if (kujiCount>=2 && atariCount<kujiCount-1)setKujiCount(v=>v-1);
+        }}>
+          くじ本数-1
+        </Button>
+        <Button color='inherit' variant="outlined" startIcon={<AddIcon />} onClick={()=>{
+          setKujiCount(v=>v+1);
+        }}>
+          くじ本数+1
+        </Button>
+      </div>
+      <div>
+        <span style={{paddingRight: '10px'}}>{`当たり数：${atariCount}個`}</span>
+        <Button color='inherit' variant="outlined" startIcon={<RemoveIcon />} onClick={()=>{
+          if (atariCount>=2)setAtariCount(v=>v-1);
+        }}>
+          当たり数-1
+        </Button>
+        <Button color='inherit' variant="outlined" startIcon={<AddIcon />} onClick={()=>{
+          if (atariCount<kujiCount-1)setAtariCount(v=>v+1);
+        }}>
+          当たり数+1
+        </Button>
       </div>
       <Grid container alignItems='center' justifyContent='center'>
         <img src='/lotteries/kujibiki_box.png' width='50%'/>
